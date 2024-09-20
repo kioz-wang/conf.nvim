4d99861cd1b515ca3b210ed958e960663eea6492
Kioz Wang <never.had@outlook.com>
2024-09-21 03:26:08 +0800

    init

---
+++ init.lua
@@ -0,0 +1,31 @@
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.cursorline = true
+
+opt.mouse = 'a'
+
+opt.clipboard = 'unnamedplus'
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = true
+
+local map = vim.keymap.set
+
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
